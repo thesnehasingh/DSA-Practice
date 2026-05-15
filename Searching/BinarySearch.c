dba48a930d37886bcb1a89d920758eeb5c0a2ff5
Sneha Singh <73370187+thesnehasingh@users.noreply.github.com>
2026-05-16 00:26:11 +0530

    Update BinarySearch.c

--- Searching/BinarySearch.c
+++ Searching/BinarySearch.c
@@ -1,1 +1,42 @@
+#include <stdio.h>
 
+int binarySearch(int arr[], int n, int key) {
+    int low = 0, high = n - 1, mid;
+
+    while (low <= high) {
+        mid = (low + high) / 2;
+
+        if (arr[mid] == key)
+            return mid;
+        else if (arr[mid] < key)
+            low = mid + 1;
+        else
+            high = mid - 1;
+    }
+    return -1;
+}
+
+int main() {
+    int n, i, key, result;
+    int arr[100];
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements in sorted order:\n", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter element to search: ");
+    scanf("%d", &key);
+
+    result = binarySearch(arr, n, key);
+
+    if (result != -1)
+        printf("Element found at index %d\n", result);
+    else
+        printf("Element not found\n");
+
+    return 0;
+}
